7b4b849eba809fd346e21b6475c4919343ecc3cc
Светлана Тумоякова <Sveta.Tumoyakova@yandex.ru>
2026-04-13 16:15:33 +0700

    refactor(account): update context

--- Context/DataBaseContext.cs
+++ Context/DataBaseContext.cs
@@ -7,8 +7,27 @@ namespace My_personal_budget_web_api.Context
     {
         public DbSet<Person> Persons { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
         public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Настройка связи один-ко-многим: User → Accounts
+            modelBuilder.Entity<Account>()
+                .HasOne(a => a.User)
+                .WithMany(u => u.Accounts)
+                .HasForeignKey(a => a.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Уникальный индекс для счёта по умолчанию у пользователя
+            modelBuilder.Entity<Account>()
+                .HasIndex(a => new { a.UserId, a.IsDefault })
+                .HasFilter("\"IsDefault\" = TRUE")
+                .IsUnique();
         }
     }
 }
